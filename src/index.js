a822dc777d64ce4b0f54b99688da3185a4b8717d
Kale Leach <kleach1212@gmail.com>
2022-05-28 19:45:48 -0700

    commit tree 4943dc096b750e499f19f5176a9de8c4ba064282

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+  // will keep all global variables here
+  const dogList = document.querySelector("#table-body")
+  const formName =document.querySelector("#dogName")
+  const formBreed =document.querySelector("#dogBreed")
+  const formSex =document.querySelector("#dogSex")
+
+
+  // will handle the fetching of all dogs 
+  function handleDogs(){
+    fetch("http://localhost:3000/dogs")
+    .then((res) => res.json())
+    .then((data) => {
+      // console.log(data) confirmed that the data was being passed
+      handleDogList(data)
+    })
+  }
+  handleDogs()
+
+  // will sort each dog
+  function handleDogList(arrOfDog){
+    const dogArr = arrOfDog
+    dogArr.map((dog) => addDogs(dog))
+  }
+
+  // will append each dog to the dom
+  function addDogs(dog){
+    const dogRow = document.createElement("tr")
+    dogRow.innerHTML = `
+    <td>${dog.name}</td>
+    <td>${dog.breed}</td>
+    <td>${dog.sex}</td>
+    <td><button id = btn_${dog.id}>Edit</button></td>
+    `
+    dogList.appendChild(dogRow)
+    
+
+    // creating within function to use scope 
+    const dogEditBtn = document.querySelector(`#btn_${dog.id}`)
+    dogEditBtn.addEventListener('click', () => {
+      formName.value = dog.name
+      formBreed.value = dog.breed
+      formSex.value = dog.sex
+    })
+  }
+
+  console.log(formName.value)
+
+
+
+
+
+
+
 })
